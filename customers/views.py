c145366893c4313c6224a72044baf504ac2663a1
Prabhjot Singh <Er.prabhjotsingh89@gmail.com>
2023-12-07 17:16:11 +0530

    updated code

--- customers/views.py
+++ customers/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 class HomePage(View):
     def get(self, request):
         template = 'customers/index.html'
-        print("The current user =======",request.user)
+        print("The current user =========",request.user)
         return render(request, template)
 
 
